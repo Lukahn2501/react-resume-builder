99956d8f04c77208f3e4395019318e4d6608b642
Lukahn2501 <kevin.roy.ic@gmail.com>
2018-10-26 17:00:43 +0200

    ADD: firebase data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 import Resume from './Components/Resume';
-import data from './dummyData/Data.json'
+import data from './dummyData/Data.json';
+import * as firebase from 'firebase';
 
 class App extends Component {
   
-  dataTest(){
-    return(
-      console.log(data.Resume.Administrative)
-    )
+  init(){
+    var config = {
+      apiKey: "AIzaSyDs0B65C_DebndcFfJ2Q7v8aL_pULGIL7E",
+      authDomain: "react-resume-builder.firebaseapp.com",
+      databaseURL: "https://react-resume-builder.firebaseio.com",
+      projectId: "react-resume-builder",
+      storageBucket: "react-resume-builder.appspot.com",
+      messagingSenderId: "1037696346073"
+    };
+    if (!firebase.apps.length) {
+      firebase.initializeApp(config);
+    } 
+    return firebase.database().ref('/Resume').on('value',function(snapshot){
+      console.log(snapshot.val())
+    });
   }
-  
+
   render() {
-    this.dataTest();
+    this.init().then((res)=>{
+      console.log(res);
+    });   
     return (
-      <div className="App">
-        <Resume resume={data.Resume}/>
-      </div>
+      <div></div>  
+      // <div className="App">
+      //   {/* <Resume resume={data.Resume}/> */}
+      // </div>
     );
   }
 }
